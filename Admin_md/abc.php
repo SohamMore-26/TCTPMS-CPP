5fe5e4ebe75b0423a68a1fbac4c586b7272d8082
RohitMore968 <125849088+RohitMore968@users.noreply.github.com>
2024-02-27 22:02:50 +0530

    ..

--- Admin_md/abc.php
+++ Admin_md/abc.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
 
 <?php
-	include "config.php";
-	$view = mysqli_query($con, "select * from academic_cal where id = '11'") or die(mysqli_error($con));
-    ?>
+include "config.php";
+$view = mysqli_query($con, "select * from academic_cal where id = '11'") or die(mysqli_error($con));
+?>
 
 <?php while ($row = mysqli_fetch_array($view)) {
-extract($row);
+    extract($row);
     $sem_duration_from = $row['sem_duration_from'];
     $sem_duration_to = $row['sem_duration_to'];
-   
-    echo " Semester Start Date : $sem_duration_from " ;
+
+    echo " Semester Start Date : $sem_duration_from ";
     echo "           ";
-    echo " Semester End Date : $sem_duration_to  " ;
-    
+    echo " Semester End Date : $sem_duration_to  ";
+
     echo "<script>";
     echo "var semDurationFrom = '$sem_duration_from';";
     echo "console.log(semDurationFrom);";
     echo "</script>";
-    }?>
+} ?>
