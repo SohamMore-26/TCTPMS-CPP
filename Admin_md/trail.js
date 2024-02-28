9469bca3dbc67865efc764d676dc82a0e43ff451
RohitMore968 <125849088+RohitMore968@users.noreply.github.com>
2024-02-28 10:59:54 +0530

    ..

--- Admin_md/trail.js
+++ Admin_md/trail.js
@@ -1,43 +1,4 @@
 
-
-// var lecDate = new Date("2024-01-02")
-
-// let datesArr=[2,3,6,9]
-// let diffArr=[]
-
-
-// for (let i = 1; i < datesArr.length; i++) 
-// {
-//     let a = datesArr[i]-datesArr[i-1]
-//     diffArr.push(a)   
-// }
-
-// let k =0;
-
-// let noOfLec = 48
-
-
-
-// for (let j = 0; j < noOfLec; j++) 
-// {
-//     let day = lecDate.getDate();
-//     let month = lecDate.getMonth()+1;
-//     let year = lecDate.getFullYear();
-//     console.log("Lec.",j+1,"    ",day.toString().padStart(2,'0'),"/",month.toString().padStart(2,'0'),"/",year.toString().padStart(2,'0'))
-//     let tempDate=lecDate.getDate()+diffArr[k]
-
-//     lecDate.setDate(tempDate)
-    
-
-//     k++
-
-//     if (k == diffArr.length) 
-//     {
-//         k=0;
-//     }
-
-// }
-
 let temp = new Date("1-2-2024")
 
 let credits = 3
