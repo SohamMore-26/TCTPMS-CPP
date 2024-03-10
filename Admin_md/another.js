0f213043b8634547b6121d86436f9ce19b44d667
RohitMore968 <125849088+RohitMore968@users.noreply.github.com>
2024-03-10 18:16:14 +0530

    dfguysdg

--- Admin_md/another.js
+++ Admin_md/another.js
@@ -6,58 +6,49 @@ let lastDate = new Date("2-21-2024")
 let dates=[]
 
 
-dates = initDates(semStartDate,dt)
+let lastLectureDate 
 
-let newDates = datesGenerate(dates,48-3,semEndDate)
+let lastEntryDate = new Date("3-6-2024")
 
-for (let i = 0; i < newDates.length; i++) {
-    console.log(newDates[i], i+1);
+let today = new Date()
+
+// dates = initDates(semStartDate,dt)
+
+// let newDates = datesGenerate(dates,48-3,semEndDate)
+
+dates = datesGenerate(45,semStartDate,semEndDate,dt)
+
+for (let i = 0; i < dates.length; i++) {
+    console.log(dates[i], i+1);
     
 }
 
-let finalDates = regenerateDates(newDates,lastDate,semEndDate,dt2)
-console.log("\n\n");
+console.log(today.toDateString())
 
-for (let i = 0; i < finalDates.length; i++) 
+
+for(let i = 0 ; i <dates.length; i++)
 {
-    console.log(finalDates[i], i+1);
     
-}
-
-
-
-
-function datesGenerate(date ,noOfLec,semEndDate) 
-{
-    let tempDate,tdate 
-
-    a : for (let i = 0; i < noOfLec; i++) 
+    if (new Date(dates[i]).getTime() > today.getTime()) 
     {
-        tdate = new Date(date[i])
-
-    
-        tempDate=tdate.getDate()+7
-
-        tdate.setDate(tempDate)
-
-        if (tdate.getTime() > semEndDate.getTime()) 
-        {
-            for (let j = 0; j < noOfLec - i; j++) {
-                date.push("Extra Lecture")
-                
-            }   
-            break a
-        }
-
-        date.push(tdate.toDateString())
-          
-
+        break
     }
-    return date
-
+    lastLectureDate = dates[i]
 }
 
-function initDates(semStartDate,dt)
+console.log("Difference : ",new Date(lastLectureDate).getDate()-lastEntryDate.getDate());
+
+// let finalDates = regenerateDates(newDates,lastDate,semEndDate,dt2)
+// console.log("\n\n");
+
+// for (let i = 0; i < finalDates.length; i++) 
+// {
+//     console.log(finalDates[i], i+1);
+    
+// }
+
+
+function datesGenerate(noOfLec,semStartDate,semEndDate,dt)
 {
     let date=[]
     let temp = semStartDate
@@ -102,33 +93,137 @@ function initDates(semStartDate,dt)
         temp.setDate(a)
     }
 
+    let tempDate,tdate 
+
+    b : for (let i = 0; i < noOfLec; i++) 
+    {
+        tdate = new Date(date[i])
+
+    
+        tempDate=tdate.getDate()+7
+
+        tdate.setDate(tempDate)
+
+        if (tdate.getTime() > semEndDate.getTime()) 
+        {
+            for (let j = 0; j < noOfLec - i; j++) {
+                date.push("Extra Lecture")
+                
+            }   
+            break b
+        }
+
+        date.push(tdate.toDateString())
+          
+
+    }
     return date
+
 }
 
-function regenerateDates(date,lastDate,semEndDate,dt2)
-{
-    let genDates,finalDates;
 
-    for(let i = 0 ; i<date.length; i++)
-    {
-        if (date[i]==lastDate.toDateString()) 
-        {
-            genDates= initDates(lastDate,dt2)
+// function datesGenerate(date ,noOfLec,semEndDate) 
+// {
+//     let tempDate,tdate 
 
-            finalDates = datesGenerate(genDates,date.length-i-3,semEndDate)
+//     a : for (let i = 0; i < noOfLec; i++) 
+//     {
+//         tdate = new Date(date[i])
 
-            date.splice(i)
+    
+//         tempDate=tdate.getDate()+7
 
-            date=date.concat(finalDates)
-            break 
-        }
-    }
+//         tdate.setDate(tempDate)
+
+//         if (tdate.getTime() > semEndDate.getTime()) 
+//         {
+//             for (let j = 0; j < noOfLec - i; j++) {
+//                 date.push("Extra Lecture")
+                
+//             }   
+//             break a
+//         }
+
+//         date.push(tdate.toDateString())
+          
+
+//     }
+//     return date
+
+// }
+
+// function initDates(semStartDate,dt)
+// {
+//     let date=[]
+//     let temp = semStartDate
+
+//     let lc
+
+//     a: for (let i = 0; i < 7; i++) 
+//     {
+      
+//         dayWeek = temp.getDay()
+
+//         switch(dayWeek)
+//         {
+//             case 1 : lc=1
+//                 break
+//             case 2 : lc=7
+//                 break
+//             case 3 : lc=13
+//                 break
+//             case 4 : lc=19
+//                 break
+//             case 5 : lc=25
+//                 break
+//             case 6 : lc=31
+//                 break
+//             case 0 :    let a = temp.getDate()+1
+//                         temp.setDate(a)
+//                         continue a
+        
+//         }
+
+
+//         for (let j = 0; j < 6; j++) 
+//         {
+//             if (dt[lc-1] == "nis") 
+//             {
+//                 date.push(temp.toDateString())     
+//             }
+//             lc++
+//         }
+//         let a = temp.getDate()+1
+//         temp.setDate(a)
+//     }
+
+//     return date
+// }
+
+// function regenerateDates(date,lastDate,semEndDate,dt2)
+// {
+//     let genDates,finalDates;
+
+//     for(let i = 0 ; i<date.length; i++)
+//     {
+//         if (date[i]==lastDate.toDateString()) 
+//         {
+//             genDates= initDates(lastDate,dt2)
+
+//             finalDates = datesGenerate(genDates,date.length-i-3,semEndDate)
+
+//             date.splice(i)
+
+//             date=date.concat(finalDates)
+//             break 
+//         }
+//     }
     
 
-    return date
+//     return date
 
 
-}
+// }
 
 
 
