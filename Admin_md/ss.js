89848a223a48bb15348b1487b3c00ff136e8d61b
RohitMore968 <125849088+RohitMore968@users.noreply.github.com>
2024-03-17 19:00:12 +0530

    hjg

--- Admin_md/ss.js
+++ Admin_md/ss.js
@@ -1,13 +1,15 @@
 let dt=["Monday","Wednesday","Friday"]
 let semStartDate = new Date("2024-01-01")
+let semEndDate = new Date("2024-04-09")
 let date1=[]
 let lecperweek=3
 let lastDate=[]
 
-lastDate = dates(48,semStartDate,dt,lecperweek)
+
+lastDate = dates(48,semStartDate,dt,lecperweek,semEndDate)
 
 for (let i = 0; i < lastDate.length; i++) {
-  console.log(i,lastDate[i])
+  console.log(i+1,lastDate[i])
   
 }
 console.log(lastDate);
@@ -15,12 +17,10 @@ console.log(lastDate);
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-function dates(noOfLec,semStartDate,dt,lecperweek)
+function dates(noOfLec,semStartDate,dt,lecperweek,semEndDate)
 {
     let date=[]
     let temp
-
-    
 
     for(let i = 0 ; i < lecperweek ; i++)
     {
@@ -29,15 +29,41 @@ function dates(noOfLec,semStartDate,dt,lecperweek)
       date.push(temp.toDateString())
         
     }
+    
     let tempDate
 
     for (let i = 0; i < noOfLec-lecperweek; i++) 
     {
       tempDate = new Date(date[i])
       
-      temp=tempDate.getDate()+7
-      tempDate.setDate(temp)
-      date.push(tempDate.toDateString())
+
+      tempDate.setDate(tempDate.getDate()+7)
+
+      if (tempDate.getTime() > semEndDate.getTime())
+      {
+
+        for (let j = 0; j < noOfLec-i-lecperweek; j++) 
+        {
+          date.push("Extra Lecture")  
+        }
+        break
+      }
+      else
+      {
+        date.push(tempDate.toDateString())
+      }
+    }
+
+    for (let i = 0; i < date.length; i++) 
+    {
+      let a = new Date(date[i])
+
+      if (date[i] == "Extra Lecture") {
+        break
+        
+      }
+      date[i]=a.getDate()+"/"+(a.getMonth()+1)+"/"+a.getFullYear()
+      
     }
     
     
--- Admin_md/tempCodeRunnerFile.js
+++ Admin_md/tempCodeRunnerFile.js
@@ -1,14 +1,5 @@
-function getNextDayFromDate(date, day) {
-//     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//     const inputDayIndex = daysOfWeek.indexOf(day);
-//     const inputDate = new Date(date);
+for (let i = 0; i < lastDate.length; i++) {
+//   console.log(i,lastDate[i])
   
-//     while (inputDate.getDay() !== inputDayIndex) {
-//       inputDate.setDate(inputDate.getDate() + 1);
-//     }
-  
-//     // Advance to the next occurrence of the given day
-//     inputDate.setDate(inputDate.getDate());
-  
-//     return inputDate;
-//   }
+// }
+// console.log(lastDate);
